9ffa58cf445ee28ffe30801bd6efff210d4eb413
BraedonWatkins <watkins.braedon@gmail.com>
2022-08-26 12:07:27 -0400

    basic login form

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'hard': '-0.2em 0.2em 0em #94A3B8',
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
   ],
   darkMode: ['class', '[data-mode="dark"]'],
 }
