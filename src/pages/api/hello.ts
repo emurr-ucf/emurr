7af065d5de54b53bee245926f045b88cf5e290e9
xTye <iwatchtyler@gmail.com>
2022-08-28 12:51:12 -0400

    Update Login types

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+interface HelloRequest {
+  name: string;
+}
+
+interface HelloResponse {
+  name: string;
+  error?: string;
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<HelloResponse>
 ) {
+  const vars: HelloRequest = req.body;
+
   res.status(200).json({ name: 'John Doe' })
 }
