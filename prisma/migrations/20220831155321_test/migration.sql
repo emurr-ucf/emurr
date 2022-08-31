d93e7299eab4ab90719ce7fb2915c1d0e70e3f9f
Edelis <edelitamr@gmail.com>
2022-08-31 12:01:28 -0400

    push to "develop" branch to update prod deployment

---
+++ prisma/migrations/20220831155321_test/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `firstName` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `lastName` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `User` DROP COLUMN `firstName`,
+    DROP COLUMN `lastName`,
+    DROP COLUMN `password`,
+    ADD COLUMN `name` VARCHAR(255) NULL;
