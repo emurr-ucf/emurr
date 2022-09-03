3f5c4c0c3087223253da5cf101c9b3702a564228
COP Group <cop43318@gmail.com>
2022-09-03 17:40:09 -0400

    Inserted Credentials, hash passwords, getUser and editUser.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID ? process.env.GITHUB_ID : "",
@@ -12,7 +12,35 @@ export default NextAuth({
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID ? process.env.GOOGLE_CLIENT_ID : "",
         clientSecret: process.env.GOOGLE_CLIENT_SECRET ? process.env.GOOGLE_CLIENT_SECRET : "",
-      }),
-    // ...add more providers here
+    }),
+    CredentialsProvider({
+			// The name to display on the sign in form (e.g. "Sign in with...").
+			name: "Credentials",
+			
+			// Credentials needed.
+			credentials: {
+				email: { label: "Email", type: "text", placeholder: "email@email.com" },
+				password: {  label: "Password", type: "password", placeholder: "Password..." }
+			},
+			async authorize(credentials, req) {
+				// API Request.
+				if(credentials) {
+					const res = await fetch("http://localhost:3000/api/user/login", {
+						method: 'POST',
+						body: JSON.stringify(credentials),
+						headers: { "Content-Type": "application/json" }
+					})
+				var user = await res.json();
+				}
+				// If API Works.
+				if(user.error === "") {
+					return user.user;
+				} 
+				else {
+					// Returning null displays a error advising the user to check their details.
+					return null
+				}
+			}
+		}),
   ],
 })
