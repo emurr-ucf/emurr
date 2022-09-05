a655c3b292d325d7e50afd02449503ad686f930f
xTye <iwatchtyler@gmail.com>
2022-09-05 16:52:43 -0400

    Working model

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,15 +1,10 @@
 import type { NextPage } from 'next';
 import { Navbar } from '../components/Navbar';
-import Router from 'next/router';
 import { useSession } from 'next-auth/react';
-import { useState } from 'react';
+import { TourSiteCard } from '../components/TourSiteCard';
 
 const DashboardPage: NextPage = () => {
   const { data: session, status } = useSession();
-
-  if (!session) {
-    Router.push('/dashboard');
-  }
 
   return (
     <>
@@ -17,13 +12,53 @@ const DashboardPage: NextPage = () => {
         <Navbar 
           page="login"
         />
-        <div className="flex w-full h-full justify-center text-green-800">
-          <div className="flex justify-center h-fit p-5 mt-28 rounded-lg bg-white shadow-hard border-2 border-green-800 sm:rounded-md">
-            {formType === FormType.LOGIN && <Login hook={handleChange} />}
-            {formType === FormType.REGISTER && <Register hook={handleChange} />}
-            {formType === FormType.FORGOT_PASSWORD && <ForgotPassword hook={handleChange} />}
-            <div className="mx-10 border-l-2 rounded border-stone-400"></div>
-            <OAuth />
+        <div className="flex w-full h-full mt-16 align-center justify-center text-green-800">
+          <div className="flex flex-col w-4/5 text-3xl gap-6">
+            <div className="flex justify-between">
+              <div>
+                Users Pages
+              </div>
+              <button className="shadow-md rounded-md px-2 bg-green-800 text-base font-bold text-white hover:bg-green-600 transition ease-in-out delay-50">
+                Create New Page +
+              </button>
+            </div>
+            <div className="flex justify-between">
+              <div className="flex w-3/5 h-auto items-center rounded-md border border-green-800 bg-white shadow-sm shadow-black">
+                <img src="/images/search.png" className="w-5 h-5 m-2" />
+                <input
+                  type="text"
+                  placeholder="Search by Title, Tour Pack, or Page Contents"
+                  className="w-full bg-transparent rounded-r-sm text-base focus:outline-none placeholder:italic placeholder:text-slate-400"
+                />
+              </div>
+              <button className="flex items-center justify-between w-36 border border-green-800 shadow-md rounded-md px-2 text-black bg-white text-base text-white hover:bg-slate-200 transition ease-in-out delay-50">
+                <div className="flex justify-center gap-2">
+                  <div className="font-bold">
+                    Sort By:
+                  </div>
+                  <div>
+                    nil
+                  </div>
+                </div>
+                <div className="text-slate-300">
+                  ▼
+                </div>
+              </button>
+            </div>
+            <div className="flex justify-between w-full">
+              <TourSiteCard
+                title="Name"
+                description="description"
+              />
+              <TourSiteCard
+                title="Name"
+                description="description"
+              />
+              <TourSiteCard
+                title="Name"
+                description="description"
+              />
+            </div>
           </div>
         </div>
       </div>
