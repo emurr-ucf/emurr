9190813135a91c745c7530a701d63fe0f7e59dba
BraedonWatkins <watkins.braedon@gmail.com>
2022-09-06 13:59:06 -0400

    navbar backdrop, formatting, & hover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export interface NavbarProps {
 export const Navbar = (props: NavbarProps) => {
   return (
     <>
-      <nav className="sticky top-0 z-50 w-full text-green border-b border-brown bg-background-500">
+      <nav className="sticky top-0 z-50 w-full text-green border-b border-brown backdrop-blur-md">
         <div className="flex justify-between h-auto w-full py-2 px-10">
           <div className="flex flex-row justify-center items-center gap-5">
             <img
@@ -20,17 +20,17 @@ export const Navbar = (props: NavbarProps) => {
             </div>
           </div>
           <div className="flex flex-row justify-center items-center text-xl">
-            <div className={`p-2 rounded-md ${props.page === "home" ? "font-semibold text-brown" : ""} hover:bg-background-700 hover:font-normal transition ease-in-out delay-50`}>
+            <div className={`p-2 rounded-md ${props.page === "home" ? "font-semibold text-brown hover:font-semibold" : ""} hover:bg-background-700 hover:font-normal transition ease-in-out delay-50`}>
               <Link href="/">
                 home
               </Link>
             </div>
-            <div className={`p-2 rounded-md ${props.page === "about" ? "font-semibold text-green" : ""} hover:bg-background-700 hover:font-normal transition ease-in-out delay-50`}>
+            <div className={`p-2 rounded-md ${props.page === "about" ? "font-semibold text-green hover:font-semibold" : ""} hover:bg-background-700 hover:font-normal transition ease-in-out delay-50`}>
               <Link href="/about" className="m-5">
                 about
               </Link>
             </div>
-            <div className={`p-2 rounded-md ${props.page === "login" ? "font-semibold text-green" : ""} hover:bg-background-700 hover:font-normal transition ease-in-out delay-50`}>
+            <div className={`p-2 rounded-md ${props.page === "login" ? "font-semibold text-green hover:font-semibold" : ""} hover:bg-background-700 hover:font-normal transition ease-in-out delay-50`}>
               <Link href="/login" className="m-5">
                 login
               </Link>
