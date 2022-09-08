a8182ed1cf44be226ade115ed517b2b37babf803
BraedonWatkins <watkins.braedon@gmail.com>
2022-09-08 19:14:45 -0400

    fix fill change width for OAuth

--- src/components/OAuth.tsx
+++ src/components/OAuth.tsx
@@ -25,7 +25,7 @@ export const OAuth = () => {
         {providers?.google && (
           <button
             onClick={() => signIn(providers.github.id)}
-            className="flex items-center justify-center gap-2 py-3 px-4  shadow-sm text-sm font-medium rounded-md text-white bg-slate-900 hover:bg-slate-700"
+            className="flex items-center justify-center gap-2 py-3 px-4 w-52 shadow-sm text-sm font-medium rounded-md text-white bg-slate-900 hover:bg-slate-700"
           >
             <div>Login with GitHub</div>
             <img src="/images/github.png" className="w-6 h-6"/>
@@ -34,7 +34,7 @@ export const OAuth = () => {
         {providers?.github && (
           <button
             onClick={() => signIn(providers.google.id)}
-            className="flex items-center justify-center gap-2 py-3 px-4  shadow-sm text-sm font-medium border border-slate-200 rounded-md text-black hover:bg-neutral-100"
+            className="flex items-center justify-center gap-2 py-3 px-4 w-52 shadow-sm text-sm font-medium border border-slate-200 rounded-md text-black hover:bg-neutral-100"
           >
             <div>Login with Google</div>
             <img src="/images/google.png" className="w-6 h-6"/>
