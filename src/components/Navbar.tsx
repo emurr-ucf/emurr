742834c3be7e0d0b362a39a78714892c12271a4b
BraedonWatkins <watkins.braedon@gmail.com>
2022-09-08 18:17:27 -0400

    "correct gap div to right side"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = (props: NavbarProps) => {
     <>
       <nav className="sticky top-0 z-50 w-full border-b border-brown backdrop-blur-md">
         <div className="flex justify-between h-auto w-full py-2 px-10">
-          <div className="flex flex-row justify-center items-center gap-5">
+          <div className="flex flex-row justify-center items-center">
             <img
               src="/images/logo_vert_5.png"
               alt="An SVG of the Emurr Logo"
@@ -19,8 +19,8 @@ export const Navbar = (props: NavbarProps) => {
               EMURR
             </div>
           </div>
-          <div className="flex flex-row justify-center items-center text-xl text-brown">
-            <div className={`p-2 rounded-md ${props.page === "home" ? "font-semibold hover:font-semibold" : ""} hover:bg-background-600 transition ease-in-out delay-50`}>
+          <div className="flex flex-row justify-center items-center text-xl text-brown gap-2">
+            <div className={`py-1 px-2 rounded-md ${props.page === "home" ? "font-semibold hover:font-semibold" : ""} hover:bg-background-600 transition ease-in-out delay-50`}>
               <Link href="/">
                 home
               </Link>
