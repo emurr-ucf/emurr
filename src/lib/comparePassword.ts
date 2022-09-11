55003d9335f58860f70acfc4134c3fa2630ecd4f
xTye <iwatchtyler@gmail.com>
2022-09-11 13:11:04 -0400

    merge conflicts

--- src/lib/comparePassword.ts
+++ src/lib/comparePassword.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const bcrypt = require("bcrypt");
-=======
 import bcrypt from "bcryptjs";
->>>>>>> sign-in-connect
 
 export default function comparePass(password: string, hashedPass: string)
 {
