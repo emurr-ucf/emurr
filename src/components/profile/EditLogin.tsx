2f85635757016c1fba36edebf7e16be64fc3d652
omosa <59900877+omosa@users.noreply.github.com>
2022-10-09 12:31:30 -0400

    Remove 'action' from props

--- src/components/profile/EditLogin.tsx
+++ src/components/profile/EditLogin.tsx
@@ -14,7 +14,7 @@ export const EditLogin = () => {
   
 	return (
 	  <>
-	    <Box image="/images/profile/mail.svg" title="Edit email" description="Edit your login email" action="" onClick={handleShow} />
+	    <Box image="/images/profile/mail.svg" title="Edit email" description="Edit your login email" onClick={handleShow} />
 		<Modal show={show} onHide={handleClose}>
 			<Modal.Header closeButton>
 				<Modal.Title>Edit email</Modal.Title>
