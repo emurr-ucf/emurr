d16f55579f9ef1f12f922e165e1b827654f98abd
Edelis <edelitamr@gmail.com>
2022-10-16 11:31:17 -0400

    add prefix "/emurr/" for production

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,8 +13,7 @@ export const Navbar = ( props: NavbarProps ) => {
     signOut();
   }
 
-  const isProduction = process.env.NODE_ENV === 'production';
-  const imagePrefix = isProduction ? "/emurr" : "";
+  const isProd = process.env.NODE_ENV === 'production';
 
   if ( status === "authenticated" ) {
     return (
@@ -22,7 +21,7 @@ export const Navbar = ( props: NavbarProps ) => {
         <nav className="sticky top-0 z-50 border-b border-brown backdrop-blur-md bg-background-400">
           <div className="flex justify-between h-auto py-2 px-10">
             <div className="flex flex-row justify-center items-center gap-4">
-              <img src={ `${ imagePrefix }/images/logo_vert_5.png` } alt="An SVG of the Emurr Logo" className="w-10 h-10" />
+              <img src={ `${ isProd ? "/emurr/images/logo_vert_5.png" : "images/logo_vert_5.png" }` } alt="An SVG of the Emurr Logo" className="w-10 h-10" />
               { props.children ?
                 props.children
                 :
@@ -35,12 +34,13 @@ export const Navbar = ( props: NavbarProps ) => {
             </div>
             <div className="flex flex-row justify-center items-center text-xl text-brown gap-2">
               <div className={ `py-1 px-2 rounded-md ${ props.page === "dashboard" ? "font-semibold hover:font-semibold" : "" } hover:bg-background-600 transition ease-in-out delay-50` }>
-                <Link href="/tours">
+                {/* <Link href="/tours"> */ }
+                <Link href={ `${ isProd ? "/emurr/tours" : "/tours" }` }>
                   dashboard
                 </Link>
               </div>
               <div className={ `py-1 px-2 rounded-md ${ props.page === "profile" ? "font-semibold hover:font-semibold" : "" } hover:bg-background-600 transition ease-in-out delay-50` }>
-                <Link href="/profile">
+                <Link href={ `${ isProd ? "/emurr/profile" : "/profile" }` }>
                   profile
                 </Link>
               </div>
@@ -68,7 +68,7 @@ export const Navbar = ( props: NavbarProps ) => {
         <div className="flex justify-between h-auto py-2 px-10">
           <div className="flex flex-row justify-center items-center gap-4">
             <img
-              src={ `${ imagePrefix }/images/logo_vert_5.png` }
+              src={ `${ isProd ? "/emurr/images/logo_vert_5.png" : "images/logo_vert_5.png" }` }
               alt="An SVG of the Emurr Logo"
               className="w-10 h-10"
             />
@@ -83,7 +83,7 @@ export const Navbar = ( props: NavbarProps ) => {
               </Link>
             </div>
             <div className={ `py-1 px-2 rounded-md ${ props.page === "about" ? "font-semibold hover:font-semibold" : "" } hover:bg-background-600 transition ease-in-out delay-50` }>
-              <Link href="/about" className="m-5">
+              <Link href={ `${ isProd ? "/emurr/about" : "/about" }` } className="m-5">
                 about
               </Link>
             </div>
