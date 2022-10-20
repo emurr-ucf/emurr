17d67f69acf9552ee1bd3c13a3e63341a342de78
BraedonWatkins <watkins.braedon@gmail.com>
2022-10-20 16:32:55 -0400

    functionally pull user data

--- src/pages/api/user/getUserId.ts
+++ src/pages/api/user/getUserId.ts
@@ -11,8 +11,12 @@ export interface GetUserRequestType {
 
 // API Outputs.
 export interface GetUserResponseType {
+<<<<<<< Updated upstream:src/pages/api/user/getUserId.ts
     user?: User;
     id?: {id: string}; // this feels very silly but i cant get it to work as pure string
+=======
+    user?: { id: string; }[];
+>>>>>>> Stashed changes:src/pages/api/user/getUserById.ts
     error: string;
 }
 
@@ -20,6 +24,7 @@ export default async function handler (
     req: NextApiRequest,
     res: NextApiResponse<GetUserResponseType>
 ) {
+<<<<<<< Updated upstream:src/pages/api/user/getUserId.ts
     const { email } = req.body;
 
     // Error: Email was not received.
@@ -37,6 +42,13 @@ export default async function handler (
         select: {
             id: true,
         },
+=======
+    // Find user information.
+    const user = await prisma.user.findMany({
+        select: {
+            id: true,
+        }
+>>>>>>> Stashed changes:src/pages/api/user/getUserById.ts
     })
 
     if(id){
---
+++ src/pages/users/[userid].tsx
@@ -0,0 +1,72 @@
+// End of Additional Extensions
+import { GetServerSideProps, GetStaticPaths, GetStaticProps, GetStaticPropsContext, InferGetServerSidePropsType, InferGetStaticPropsType, NextPage} from "next";
+import { User } from '@prisma/client';
+import { getToken } from 'next-auth/jwt';
+import { prisma } from "../../lib/prisma";
+import { debug } from "console";
+import { ParsedUrlQuery } from "querystring";
+import { propTypesSelected } from "@material-tailwind/react/types/components/select";
+// import { getData } from './api/hello';
+
+
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+    const res = await fetch('http://localhost:3000/api/user/getUserById', {
+        // method: "POST"
+    }); //idk what to do for this tbh
+    const data = await res.json();
+    console.log(data);
+
+    const paths = data.user.map((user: User) => {
+        return{
+            params: {userid: user.id.toString()}
+        };
+    })
+
+    return{
+        paths,
+        fallback: false // to stop from bad ids routing
+    }
+}
+
+interface jk {
+    user?: User,
+}
+interface jk2 { 
+}
+
+interface Params extends ParsedUrlQuery {
+    userid: string;
+}
+
+// \[T]/ //praise the sun
+
+//ignoring types
+export const getStaticProps:GetStaticProps<jk, Params> = async (context) => {
+    const { userid } = context.params!;
+
+    if(!userid) return { props: {user: undefined} }
+
+
+     // Find user information.
+     const user = await prisma.user.findFirst({
+        where: {
+            id: userid,
+        },
+        select: { 
+            registeredAt: false,
+            name: true,
+        }
+    })
+    return { props: {user}}
+}
+
+const UsersPage:NextPage = ({ user }: InferGetStaticPropsType<typeof getStaticProps>) => {
+    return(
+        <>
+            <h1>{user?.name}</h1>
+        </>
+    )
+};
+
+export default UsersPage;
