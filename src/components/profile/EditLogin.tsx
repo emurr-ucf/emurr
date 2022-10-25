4e8c213151ae4238baa5f1c00746f96da7b8e3ab
edelis <edelitamr@gmail.com>
2022-10-25 12:22:05 -0400

    prefixing images with /emurr

--- src/components/profile/EditLogin.tsx
+++ src/components/profile/EditLogin.tsx
@@ -3,47 +3,49 @@ import { useState } from "react";
 import { Box } from '../Box';
 
 export const EditLogin = () => {
-	const [show, setShow] = useState(false);
-	const handleShow = () => setShow(true);
-	const handleClose = () => setShow(false);
+	const [ show, setShow ] = useState( false );
+	const handleShow = () => setShow( true );
+	const handleClose = () => setShow( false );
 	const handleSave = () => {
 		// TODO make a request and show response
-		alert("Submitted");
-		setShow(false);
+		alert( "Submitted" );
+		setShow( false );
 	}
-  
+
+	const isProd = process.env.NODE_ENV === 'production';
+
 	return (
-	  <>
-	    <Box image="/images/key.png" title="Edit email" description="Edit your login email" action="" onClick={handleShow} />
-		<Modal show={show} onHide={handleClose}>
-			<Modal.Header closeButton>
-				<Modal.Title>Edit email</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-			<form onSubmit={handleSave}>
-				<div className="flex flex-col gap-6">
-					<input 
-						type="text"
-						placeholder='Email'
-						className="h-12 appearance-none border border-brown rounded px-3"
-					/>
-					<input
-						type="password"
-						placeholder='Verify your password'
-						className="h-12 appearance-none border border-brown rounded px-3"
-					/>
-					<div className="flex justify-center">
-						<button
-							type="submit"
-							className="py-3 px-4 w-3/4 shadow-sm text-sm font-medium rounded-md text-background-200 bg-green-700 hover:bg-green-800"
-						>
-							Save
-						</button>
-					</div>
-				</div>
-			</form>
-			</Modal.Body>
-		</Modal>
-  	  </>
+		<>
+			<Box image={ `${ isProd ? "/emurr/images/key.png" : "/images/key.png" } ` } title="Edit email" description="Edit your login email" action="" onClick={ handleShow } />
+			<Modal show={ show } onHide={ handleClose }>
+				<Modal.Header closeButton>
+					<Modal.Title>Edit email</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<form onSubmit={ handleSave }>
+						<div className="flex flex-col gap-6">
+							<input
+								type="text"
+								placeholder='Email'
+								className="h-12 appearance-none border border-brown rounded px-3"
+							/>
+							<input
+								type="password"
+								placeholder='Verify your password'
+								className="h-12 appearance-none border border-brown rounded px-3"
+							/>
+							<div className="flex justify-center">
+								<button
+									type="submit"
+									className="py-3 px-4 w-3/4 shadow-sm text-sm font-medium rounded-md text-background-200 bg-green-700 hover:bg-green-800"
+								>
+									Save
+								</button>
+							</div>
+						</div>
+					</form>
+				</Modal.Body>
+			</Modal>
+		</>
 	);
 }
--- src/components/profile/EditProfile.tsx
+++ src/components/profile/EditProfile.tsx
@@ -3,58 +3,60 @@ import { useState } from "react";
 import { Box } from '../Box';
 
 export const EditProfile = () => {
-	const [show, setShow] = useState(false);
-	const handleShow = () => setShow(true);
-	const handleClose = () => setShow(false);
+	const [ show, setShow ] = useState( false );
+	const handleShow = () => setShow( true );
+	const handleClose = () => setShow( false );
 	const handleSave = () => {
 		// TODO make a request and show response
-		alert("Submitted");
-		setShow(false);
+		alert( "Submitted" );
+		setShow( false );
 	}
-  
-	return (
-	  <>
-	    <Box image="/images/key.png" title="Edit profile" description="Edit your profile information, such as your name and profile picture" action="" onClick={handleShow} />
-		<Modal show={show} onHide={handleClose}>
-			<Modal.Header closeButton>
-				<Modal.Title>Edit profile</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-			<form onSubmit={handleSave}>
-				<div className="flex flex-col gap-6">
-					<label htmlFor="pfp">
-						<div className='filter hover:contrast-200 m-auto' style={{
-							backgroundImage: "url('/images/default-user.png')",
-							backgroundSize: "128px 128px",
-							height: 128,
-							width: 128
-						}}>
-						</div>
-					</label>
-					<input id="pfp" type="file" className="hidden" />
 
-					<input 
-						type="text"
-						placeholder='First name'
-						className="h-12 appearance-none border border-brown rounded px-3"
-					/>
-					<input 
-						type="text"
-						placeholder='Last name'
-						className="h-12 appearance-none border border-brown rounded px-3"
-					/>
-					<div className="flex justify-center">
-						<button
-							type="submit"
-							className="py-3 px-4 w-3/4 shadow-sm text-sm font-medium rounded-md text-background-200 bg-green-700 hover:bg-green-800"
-						>
-							Save
-						</button>
-					</div>
-				</div>
-			</form>
-			</Modal.Body>
-		</Modal>
-  	  </>
+	const isProd = process.env.NODE_ENV === 'production';
+
+	return (
+		<>
+			<Box image={ `${ isProd ? "/emurr/images/key.png" : "/images/key.png" } ` } title="Edit profile" description="Edit your profile information, such as your name and profile picture" action="" onClick={ handleShow } />
+			<Modal show={ show } onHide={ handleClose }>
+				<Modal.Header closeButton>
+					<Modal.Title>Edit profile</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<form onSubmit={ handleSave }>
+						<div className="flex flex-col gap-6">
+							<label htmlFor="pfp">
+								<div className='filter hover:contrast-200 m-auto' style={ {
+									backgroundImage: "url('/images/default-user.png')",
+									backgroundSize: "128px 128px",
+									height: 128,
+									width: 128
+								} }>
+								</div>
+							</label>
+							<input id="pfp" type="file" className="hidden" />
+
+							<input
+								type="text"
+								placeholder='First name'
+								className="h-12 appearance-none border border-brown rounded px-3"
+							/>
+							<input
+								type="text"
+								placeholder='Last name'
+								className="h-12 appearance-none border border-brown rounded px-3"
+							/>
+							<div className="flex justify-center">
+								<button
+									type="submit"
+									className="py-3 px-4 w-3/4 shadow-sm text-sm font-medium rounded-md text-background-200 bg-green-700 hover:bg-green-800"
+								>
+									Save
+								</button>
+							</div>
+						</div>
+					</form>
+				</Modal.Body>
+			</Modal>
+		</>
 	);
 }
