b3ee1a7af4c7b00e63610f3cfe5e6b1041f551dc
Edelis <edelitamr@gmail.com>
2022-10-30 12:26:52 -0400

    fix tour not populating

--- src/pages/tours/index.tsx
+++ src/pages/tours/index.tsx
@@ -1,24 +1,23 @@
-import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next';
-import { Navbar } from '../../components/Navbar';
-import { useSession } from 'next-auth/react';
-import { TourSiteCard } from '../../components/TourSiteCard';
-import Router from 'next/router';
-import { Tour } from '@prisma/client';
-import { getToken } from 'next-auth/jwt';
+import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next";
+import { Navbar } from "../../components/Navbar";
+import { useSession } from "next-auth/react";
+import { TourSiteCard } from "../../components/TourSiteCard";
+import Router from "next/router";
+import { Tour } from "@prisma/client";
+import { getToken } from "next-auth/jwt";
 import { prisma } from "../../lib/prisma";
-import { CreateTourResponseType } from '../api/tour';
-import { useEffect, useState } from 'react';
+import { CreateTourResponseType } from "../api/tour";
+import { useEffect, useState } from "react";
 
-import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import React from 'react'
-import { title } from 'process';
-import { urlPath } from '../../lib/urlPath';
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import React from "react";
+import { title } from "process";
+import { urlPath } from "../../lib/urlPath";
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
-
 
 const DashboardPage: NextPage = ({ propTours }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const { data: session, status } = useSession();
@@ -32,18 +31,17 @@ const DashboardPage: NextPage = ({ propTours }: InferGetServerSidePropsType<type
 
     timer = setTimeout(async () => {
       const res = await fetch(`${urlPath}/api/tour?sortQuery=${sortQuery}&query=${query}`, {
-        method: "GET"
-      })
+        method: "GET",
+      });
       const resJSON = await res.json();
 
-      if (resJSON)
-        setTours(resJSON.tours);
-    }, 500)
-  }
+      if (resJSON) setTours(resJSON.tours);
+    }, 500);
+  };
 
   useEffect(() => {
     queryTours();
-  }, [query, sortQuery])
+  }, [query, sortQuery]);
 
   if (status === "loading") return <div>Loading...</div>;
   if (status === "unauthenticated") Router.push("/");
@@ -51,15 +49,11 @@ const DashboardPage: NextPage = ({ propTours }: InferGetServerSidePropsType<type
   return (
     <>
       <div className="w-full min-h-screen">
-        <Navbar
-          page="dashboard"
-        />
+        <Navbar page="dashboard" />
         <div className="flex w-full h-full mt-16 align-center justify-center pb-20">
           <div className="flex flex-col w-4/5 text-3xl gap-6">
             <div className="flex justify-between">
-              <div>
-                Users Pages
-              </div>
+              <div>Users Pages</div>
               <button
                 onClick={async () => {
                   const file = new File([], "blank.html");
@@ -73,8 +67,7 @@ const DashboardPage: NextPage = ({ propTours }: InferGetServerSidePropsType<type
 
                   const body: CreateTourResponseType = await res.json();
 
-                  if (!body.error)
-                    Router.push("/tours/${body.tourId}");
+                  if (!body.error) Router.push(`/tours/${body.tourId}`);
                 }}
                 className="shadow-md rounded-md px-2 bg-green-800 text-base font-bold text-white hover:bg-green-600 transition ease-in-out delay-50"
               >
@@ -94,9 +87,7 @@ const DashboardPage: NextPage = ({ propTours }: InferGetServerSidePropsType<type
               <Menu as="div" className="relative inline-block text-left">
                 <div>
                   <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
-                    <div className="font-bold">
-                      Sort By: {sortQuery} ▾
-                    </div>
+                    <div className="font-bold">Sort By: {sortQuery} ▾</div>
                   </Menu.Button>
                 </div>
                 <Transition
@@ -110,24 +101,30 @@ const DashboardPage: NextPage = ({ propTours }: InferGetServerSidePropsType<type
                 >
                   <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <div className="py-1">
-                      <form onClick={(event) => setSortQuery(' ')}>
+                      <form onClick={(event) => setSortQuery(" ")}>
                         <Menu.Item>
                           {({ active }) => (
-                            <a href="#" className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm')}>None</a>
+                            <a href="#" className={classNames(active ? "bg-gray-100 text-gray-900" : "text-gray-700", "block px-4 py-2 text-sm")}>
+                              None
+                            </a>
                           )}
                         </Menu.Item>
                       </form>
-                      <form onClick={(event) => setSortQuery('Title')}>
+                      <form onClick={(event) => setSortQuery("Title")}>
                         <Menu.Item>
                           {({ active }) => (
-                            <a href="#" className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm')}>Title</a>
+                            <a href="#" className={classNames(active ? "bg-gray-100 text-gray-900" : "text-gray-700", "block px-4 py-2 text-sm")}>
+                              Title
+                            </a>
                           )}
                         </Menu.Item>
                       </form>
-                      <form onClick={(event) => setSortQuery('Date')}>
+                      <form onClick={(event) => setSortQuery("Date")}>
                         <Menu.Item>
                           {({ active }) => (
-                            <a href="#" className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm')}>Date Created</a>
+                            <a href="#" className={classNames(active ? "bg-gray-100 text-gray-900" : "text-gray-700", "block px-4 py-2 text-sm")}>
+                              Date Created
+                            </a>
                           )}
                         </Menu.Item>
                       </form>
@@ -138,20 +135,15 @@ const DashboardPage: NextPage = ({ propTours }: InferGetServerSidePropsType<type
             </div>
             <div className="inline-grid grid-cols-3 justify-items-center gap-6">
               {tours.map((tour: Tour) => {
-                return <TourSiteCard
-                  id={tour.id}
-                  key={tour.id}
-                  title={tour.tourTitle}
-                  description={ tour.tourDescription ? tour.tourDescription : "No description..." }
-                />
+                return <TourSiteCard id={tour.id} key={tour.id} title={tour.tourTitle} description={tour.tourDescription ? tour.tourDescription : "No description..."} />;
               })}
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const token = await getToken(context);
@@ -165,17 +157,17 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         id: true,
         tourTitle: true,
         tourDescription: true,
-      }
+      },
     });
 
     return {
-      props: { propTours }
-    }
+      props: { propTours },
+    };
   }
 
   return {
-    props: { propTours: [] }
-  }
-}
+    props: { propTours: [] },
+  };
+};
 
-export default DashboardPage
+export default DashboardPage;
