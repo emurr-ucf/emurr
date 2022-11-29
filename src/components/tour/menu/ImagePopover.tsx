3464ced059780c783e3d1949cac3eb4f1771e301
Edelis <edelitamr@gmail.com>
2022-11-29 09:46:55 -0500

    mouse over display title in Insert image

--- src/components/tour/menu/ImagePopover.tsx
+++ src/components/tour/menu/ImagePopover.tsx
@@ -15,14 +15,7 @@ interface ImagePopoverProps {
   getImages: (alert?: Id) => {};
 }
 
-export const ImagePopover = ({
-  tourId,
-  setTour,
-  mediaSize,
-  editor,
-  images,
-  getImages,
-}: ImagePopoverProps) => {
+export const ImagePopover = ({ tourId, setTour, mediaSize, editor, images, getImages }: ImagePopoverProps) => {
   const imageLoad = () => {
     if (images.length > 0)
       return images.map((image: TourSiteImageType) => (
@@ -54,9 +47,7 @@ export const ImagePopover = ({
             }}
             className="flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-background-500 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
           >
-            <div className="text-sm font-medium text-gray-900">
-              {image.name}
-            </div>
+            <div className="text-sm font-medium text-gray-900">{image.name}</div>
           </button>
         </div>
       ));
@@ -102,12 +93,7 @@ export const ImagePopover = ({
         {({ open }) => (
           <>
             <Popover.Button className="flex justify-center items-center w-7 h-7">
-              <img
-                src={`${urlLocalPath}/images/image-line.svg`}
-                alt="image"
-                title="Insert image"
-                className="w-7 h-7 p-1 hover:bg-background-400 rounded transition ease-in-out"
-              />
+              <img src={`${urlLocalPath}/images/image-line.svg`} alt="image" title="Insert image or video" className="w-7 h-7 p-1 hover:bg-background-400 rounded transition ease-in-out" />
             </Popover.Button>
             <Transition
               as={Fragment}
@@ -120,30 +106,17 @@ export const ImagePopover = ({
             >
               <Popover.Panel className="fixed z-20 mt-3 max-w-md px-4 right-0">
                 <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                  <div className="flex-col gap-8 bg-white px-7 py-2 h-40 overflow-y-auto">
-                    {imageLoad()}
-                  </div>
+                  <div className="flex-col gap-8 bg-white px-7 py-2 h-40 overflow-y-auto">{imageLoad()}</div>
                   <div className="bg-gray-50 p-4">
-                    <div className="text-sm text-gray-500 text-right">
-                      Total: {mediaSize.toFixed(1)} MB
-                    </div>
+                    <div className="text-sm text-gray-500 text-right">Total: {mediaSize.toFixed(1)} MB</div>
                     <label htmlFor="media-file">
                       <div className="rounded-md px-2 py-2 hover:cursor-pointer hover:bg-background-500 transition duration-150 ease-in-out focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
                         <div className="flex items-center">
-                          <div className="text-sm font-medium text-gray-900">
-                            Upload
-                          </div>
+                          <div className="text-sm font-medium text-gray-900">Upload</div>
                         </div>
-                        <div className="block text-sm text-gray-500">
-                          Upload an image or video.
-                        </div>
+                        <div className="block text-sm text-gray-500">Upload an image or video.</div>
                       </div>
-                      <input
-                        id="media-file"
-                        type="file"
-                        onChange={uploadImage}
-                        className="hidden"
-                      />
+                      <input id="media-file" type="file" onChange={uploadImage} className="hidden" />
                     </label>
                   </div>
                 </div>
