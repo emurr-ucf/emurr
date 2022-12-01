2e6a8ee7a4d331d79fbce23da6d884ab38e4a06c
BraedonWatkins <watkins.braedon@gmail.com>
2022-12-01 07:12:52 -0500

    dynamic anims

--- src/components/util/Logo.tsx
+++ src/components/util/Logo.tsx
@@ -3,9 +3,14 @@ import { urlLocalPath } from "../../lib/urlPath";
 
 export const Logo = () => {
   const [logo, setLogo] = useState(true);
+  const [anim, setAnim] = useState(true);
 
-  const clicked = () => {
+  const logoClicked = () => {
     setLogo(!logo);
+  };
+
+  const animClicked = () => {
+    setAnim(!anim);
   };
 
   function setImagePrefix() {
@@ -17,12 +22,18 @@ export const Logo = () => {
     <>
       <div>
         <div className="rounded-full flex items-center justify-center relative">
-          <div className="rounded-full h-36 absolute w-36 animate-ping hover:bg-green-500 hover:z-0" />
           <img
-            className="rounded-full w-28 h-28 self-center cursor-pointer m-0 z-50"
+            className="rounded-full w-28 h-28 self-center cursor-pointer m-0"
             src={setImagePrefix()}
             alt="outline of an NFC tag. when clicked becomes a pie."
-            onClick={clicked}
+            onClick={logoClicked}
+          />
+          <div
+            className={
+              "cursor-pointer border-10 border-blue-300 rounded-full h-36 absolute w-36" +
+              (anim ? " bg-green-500 animate-ping" : " bg-transparent")
+            }
+            onClick={animClicked}
           />
         </div>
       </div>
