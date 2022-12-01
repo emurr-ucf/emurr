9615fef78073e65a52954efd61584b7dda93f051
omosa <59900877+omosa@users.noreply.github.com>
2022-12-01 14:25:37 -0500

    styling updates to user cards

--- src/components/user/UserCard.tsx
+++ src/components/user/UserCard.tsx
@@ -9,29 +9,28 @@ interface UserCardProps {
 }
 
 export const UserCard = (props: UserCardProps) => {
+  const user = props.user;
   return (
     <>
-      <Link href={`${urlLocalPath}/users/${props.user.id}`}>
+      <Link href={`${urlLocalPath}/users/${user.id}`}>
         <div className="flex justify-center h-40 w-11/12 align-middle rounded-lg bg-white shadow-md shadow-slate-400 border-2 border-stone-500 sm:rounded-md transition ease-in-out hover:bg-grey hover:cursor-pointer hover:scale-110 hover:animate-pulse select-none">
-          <div className="flex flex-col h-full w-full justify-between p-2">
+          <div className="flex flex-col h-full w-full justify-between p-2 min-width-0">
             <div className="flex flex-col w-full">
               <div className="text-2xl">
-                {`${props.user.name} ${props.user.lastName || ""}`}
+                {user.name + (user.lastName ? " " + user.lastName : "")}
               </div>
-              <div className="text-sm overflow-y-hidden">
-                {props.user.email || ""}
+              <div className="text-sm overflow-y-hidden overflow-x-hidden">
+                {user.email || ""}
               </div>
             </div>
             <div className="flex justify-between items-center w-full text-sm">
-              {props.user.role === "ADMIN" ? "Administrator" : "User"}
-            </div>
-          </div>
-          <div>
-            {props.user.role === "ADMIN" ? 
-              <img src={`${urlLocalPath}/images/profile/shield.svg`} height={64} width={64} title="Administrator" alt="Administrator" />
+              {user.role === "ADMIN" ? "Administrator" : "User"}
+              {user.role === "ADMIN" ? 
+              <img src={`${urlLocalPath}/images/profile/shield.svg`} height={32} width={32} title="Administrator" alt="Administrator" />
               :
               <></>
-            }
+              }
+            </div>
           </div>
         </div>
       </Link>
