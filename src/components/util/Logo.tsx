bdca7c764170871ec968250db7e33290f81f7cd9
BraedonWatkins <watkins.braedon@gmail.com>
2022-12-01 06:53:20 -0500

    simple fix

--- src/components/util/Logo.tsx
+++ src/components/util/Logo.tsx
@@ -16,16 +16,17 @@ export const Logo = () => {
   return (
     <>
       <div>
-        <div className="flex items-center justify-center relative">
+        <div className="rounded-full flex items-center justify-center relative border-2 border-red-300">
+          <div className="border-2 border-sky-500 rounded-full h-36 absolute w-36 animate-ping hover:bg-green-500 hover:z-0" />
           <img
-            className="z-40 w-28 h-28 self-center cursor-pointer"
+            className="border-2 border-emerald-300 rounded-full w-28 h-28 self-center cursor-pointer m-0 z-50"
             src={setImagePrefix()}
-            alt=""
+            alt="outline of an NFC tag. when clicked becomes a pie."
             onClick={clicked}
           />
-          <div className="bg-green-500 rounded-full h-36 absolute w-36 hover:animate-ping"></div>
         </div>
       </div>
     </>
   );
 };
+
