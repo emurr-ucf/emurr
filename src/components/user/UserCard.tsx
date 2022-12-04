93f2af7563b4d611bc38457ffbcc049f71e85bed
omosa <59900877+omosa@users.noreply.github.com>
2022-12-03 21:10:10 -0500

    Bold logged in account in /admin

--- src/components/user/UserCard.tsx
+++ src/components/user/UserCard.tsx
@@ -3,6 +3,7 @@ import { urlLocalPath, urlPath } from "../../lib/urlPath";
 import { User } from "@prisma/client";
 import { MouseEventHandler } from "react";
 import { userAgent } from "next/server";
+import { useUserStore } from "../../lib/store/user";
 
 interface UserCardProps {
   user: User;
@@ -10,11 +11,16 @@ interface UserCardProps {
 
 export const UserCard = (props: UserCardProps) => {
   const user = props.user;
+  const email = useUserStore((state) => state.email);
+  const isSelfUser = email === user.email;
+  const url = isSelfUser ? `${urlLocalPath}/tours` : `${urlLocalPath}/users/${user.id}`
+  const bold = isSelfUser ? " font-bold" : "";
+
   return (
     <>
-      <Link href={`${urlLocalPath}/users/${user.id}`}>
+      <Link href={url}>
         <div className="flex justify-center h-40 w-11/12 align-middle rounded-lg bg-white shadow-md shadow-slate-400 border-2 border-stone-500 sm:rounded-md transition ease-in-out hover:bg-grey hover:cursor-pointer hover:scale-110 hover:animate-pulse select-none">
-          <div className="flex flex-col h-full w-full justify-between p-2 min-width-0">
+          <div className={"flex flex-col h-full w-full justify-between p-2 min-width-0" + bold}>
             <div className="flex flex-col w-full">
               <div className="text-2xl">
                 {user.name + (user.lastName ? " " + user.lastName : "")}
